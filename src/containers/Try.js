3d21b6a23a2f42112f1805790a79d7954ad2c7e2
millasml <millasml@yahoo.com.sg>
2021-10-21 02:36:17 +0800

    Halfway done try section

--- src/containers/Try.js
+++ src/containers/Try.js
@@ -2,6 +2,10 @@ import "./Try.scss";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 import Section from "../components/Section";
 
@@ -18,7 +22,42 @@ export default function Timeline() {
       }
     >
       <Row>
-        <Col>hellos</Col>
+        <Col>
+          we are calling for <strong>female</strong> students for{" "}
+          <strong>one</strong> project:
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Container>
+              <Row>
+                <Col>
+                  <div className="title">CareerSocius Platform</div>
+                  <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce viverra magna ac tellus aliquam, non vestibulum tortor
+                    condimentum. Ut vehicula accumsan condimentum. Praesent eu
+                    lacinia lectus, vitae scelerisque massa. Mauris porttitor
+                    dictum nibh, ac suscipit metus congue vitae.
+                  </div>
+                </Col>
+                <Col>
+                  {" "}
+                  <div className="sub-title">Mentor</div>
+                  <div>Milla Samuel</div>
+                  <div className="sub-title">Looking For</div>
+                  <div>
+                    <Badge>Software Engineer</Badge>{" "}
+                    <Badge>Product Manager</Badge>{" "}
+                    <Badge>Head of Product</Badge>
+                  </div>
+                  <Button>Apply Now</Button>
+                </Col>
+              </Row>
+            </Container>
+          </Card>
+        </Col>
       </Row>
     </Section>
   );
