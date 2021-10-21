b0a8e0a9da314910250fc458895cf3b6f5f73193
millasml <millasml@yahoo.com.sg>
2021-10-22 02:40:25 +0800

    Add socials to AppBar

--- src/containers/AppBar.js
+++ src/containers/AppBar.js
@@ -3,6 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
+import {
+  faLinkedin,
+  faFacebook,
+  faTelegram,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function AppBar() {
   return (
     <Navbar expand="lg" sticky="top" className="navbar-dark app-bar">
@@ -14,6 +22,18 @@ export default function AppBar() {
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#timeline">Timeline</Nav.Link>
             <Nav.Link href="#signup">Sign Up</Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/company/trycatchclub/">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Nav.Link>
+            <Nav.Link href="https://www.facebook.com/groups/1053470348839870">
+              <FontAwesomeIcon icon={faFacebook} />
+            </Nav.Link>
+            <Nav.Link href="https://t.me/trycatchclub">
+              <FontAwesomeIcon icon={faTelegram} />
+            </Nav.Link>
+            <Nav.Link href="https://www.instagram.com/trycatchclub/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
