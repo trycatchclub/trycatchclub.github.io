0b4e507536fb8fea11d4d0cd169feb5844dee56f
millasml <millasml@yahoo.com.sg>
2022-04-15 09:24:47 +0800

    Created route to projects/careersocius

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing";
+import CareerSociusProject from "./pages/projects/careersocius";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <Router>
+      <Routes>
+        <Route
+          path="/projects/careersocius"
+          element={<CareerSociusProject />}
+        />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/pages/projects/careersocius.js
@@ -0,0 +1,3 @@
+export default function CareerSociusProject() {
+  return <div>hello</div>;
+}
