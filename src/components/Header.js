fede9e8c0b67b51aba52bb1390e28eda0590783c
millasml <millasml@yahoo.com.sg>
2022-04-16 01:05:47 +0800

    Forward className in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import "./Header.scss";
 
-export default function Header({ children }) {
+export default function Header({ children, className }) {
   return (
-    <div className="custom-header">
+    <div className={`custom-header ${className}`}>
       <strong>{`{`}</strong>
       {children}
       <strong>{`}`}</strong>
